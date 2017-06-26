3defb1a475476868da8885e52d37725e8ee9910d
angel_banuelos <ing.angel.banuelos@gmail.com>
2017-06-25 23:09:38 -0500

    Working with face recognition MapRed

---
+++ tools/face/src/main/java/org/hipi/tools/face/FaceRecognitionReducer.java
@@ -0,0 +1,30 @@
+package org.hipi.tools.face;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+
+public class FaceRecognitionReducer extends Reducer<Text, IntWritable, Text, Text> {
+	private Text result = new Text();
+	private Text folder = new Text();
+
+	@Override
+	public void reduce(Text key, Iterable<IntWritable> values, Context context)
+			throws IOException, InterruptedException {
+		
+		int totalFoundFaces = 0;
+		int size = 0;
+		//Grouping each key and counting all the occurrences.
+		for (IntWritable value : values) {
+			totalFoundFaces += value.get();
+			size++;
+		}
+		result.set("Analyzed Images " + size + " Found faces " + totalFoundFaces);
+		//Saving the value in the given HDFS directory.
+		context.write(key, result);
+		
+	}
+}
---
+++ tools/face/src/main/java/org/hipi/tools/face/HibFace.java
@@ -0,0 +1,63 @@
+package org.hipi.tools.face;
+
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.Parser;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.ToolRunner;
+
+public class HibFace extends Configured {
+
+	private static final Options options = new Options();
+	private static final Parser parser = (Parser) new BasicParser();
+
+	static {
+		options.addOption("f", "force", false, "force overwrite if output HIB already exists");
+		options.addOption("h", "hdfs-input", false, "assume input directory is on HDFS");
+		options.addOption("a", "action", true, "faceDetection FD, FaceRecognition FR, its a Must");
+		options.addOption("m", "recognition-method", true,
+				"LBPHFaceRecognizer = 1, FisherFaceRecognizer = 2," + " EigenFaceRecognizer = 3 ");
+		options.addOption("mp", "image-limit-percentage", true,
+				"Maximun number of images to be load per folder by percentage");
+	}
+
+	private static void usage() {
+		// usage
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp("face.jar [options] <args>", options);
+		System.exit(0);
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		CommandLine line = null;
+		String action = null;
+
+		try {
+			line = parser.parse(options, args);
+		} catch (ParseException exp) {
+			usage();
+		}
+		if (line == null) {
+			usage();
+		}
+		if (!line.hasOption("a")) {
+			usage();
+		} else {
+			action = line.getOptionValue("a");
+		}
+		if (action != null && action.equalsIgnoreCase("FD")) {
+			ToolRunner.run(new FaceDetection(), args);
+		} else if (action != null && action.equalsIgnoreCase("FR")) {
+			ToolRunner.run(new FaceRecognition(), args);
+		} else {
+			usage();
+		}
+		
+		System.exit(0);
+	}
+
+}
