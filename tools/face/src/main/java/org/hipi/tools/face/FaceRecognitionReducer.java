15a7398e3146c409a9b9a4951d9ff5e104b67bcf
Angel Banuelos <ing.angel.banuelos@gmail.com>
2017-07-05 17:49:45 -0500

    Trying to do the MapRed :(

--- tools/face/src/main/java/org/hipi/tools/face/FaceRecognitionReducer.java
+++ tools/face/src/main/java/org/hipi/tools/face/FaceRecognitionReducer.java
@@ -1,17 +1,21 @@
 package org.hipi.tools.face;
 
 import java.io.IOException;
+import java.nio.IntBuffer;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Reducer.Context;
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_core.Mat;
+import org.bytedeco.javacpp.opencv_core.MatVector;
 import org.hipi.opencv.OpenCVMatWritable;
 
 public class FaceRecognitionReducer extends Reducer<Text, OpenCVMatWritable, Text, Text> {
 	private Text result = new Text();
 	private Text folder = new Text();
-	private long id = 0L;
+	private int id = 0;
 
 	@Override
 	public void reduce(Text key, Iterable<OpenCVMatWritable> values, Context context)
@@ -22,9 +26,21 @@ public class FaceRecognitionReducer extends Reducer<Text, OpenCVMatWritable, Tex
 		for (OpenCVMatWritable value : values) {
 			totalImagesPerFace++;
 		}
+		
 		key = new Text(key + "_" + id++);
 		result.set("Images found for " + key +" are :  " + totalImagesPerFace);
 		//Saving the value in the given HDFS directory.
+		MatVector images = new MatVector(totalImagesPerFace);
+		Mat labels = new Mat(totalImagesPerFace, 1, opencv_core.CV_32SC1);
+		IntBuffer labelsBuf = labels.createBuffer();
+		
+		int counter = 0;
+		for (OpenCVMatWritable value : values) {
+			images.put(counter, value.getMat());
+			labelsBuf.put(counter, id);
+			counter++;
+		}
+		
 		context.write(key, result);
 		
 	}
